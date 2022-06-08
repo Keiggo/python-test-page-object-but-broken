eee20c0c5f10996dab822b95d300d6763218967b
Keiggo <105018856+Keiggo@users.noreply.github.com>
2022-06-08 14:16:17 +0100

    Page object model but working!

---
+++ Grain Supplier Solution - page object model/grains/tests/testloginpagetaborder.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from pages.loginpage import LoginPage
+
+#login page tab order accessibility test
+#arrange
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('http://localhost:8000/admin/')
+driver.maximize_window()
+loginPage = LoginPage(driver)
+
+#ensure that the field selected by default is the email field
+assert loginPage.getCurrentlySelectedElement() == loginPage.emailField()
+
+#act
+#assert
+loginPage.checkLoginPageElementsInRightOrder()
+
+#teardown
+driver.quit()
