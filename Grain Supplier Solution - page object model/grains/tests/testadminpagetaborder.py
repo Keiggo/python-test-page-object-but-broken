9e6460e2bb23de700c650dd628efb03e4e098057
Keiggo <105018856+Keiggo@users.noreply.github.com>
2022-06-08 14:48:50 +0100

    Add files via upload

---
+++ Grain Supplier Solution - page object model/grains/tests/testadminpagetaborder.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from pages.adminpage import AdminPage
+from pages.loginpage import LoginPage
+
+#admin page tab order accessibility test
+#arrange
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('http://localhost:8000/admin/')
+driver.maximize_window()
+loginPage = LoginPage(driver)
+adminPage = AdminPage(driver)
+
+#act
+loginPage.hideDjdtPane()
+loginPage.logInAsAdmin()
+
+#assert
+#this tabs through all selectable page element and asserts that each one is in the correct order
+adminPage.checkAdminPageElementsAreInCorrectOrder()
+
+#teardown
+driver.quit()
